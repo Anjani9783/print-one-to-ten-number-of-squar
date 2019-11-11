de044bce149508c7610fa24e3ca920f79a719f0a
Anjani9783 <54346036+Anjani9783@users.noreply.github.com>
2019-11-12 00:44:07 +0530

    Update firsttoten.cpp

--- firsttoten.cpp
+++ firsttoten.cpp
@@ -4,11 +4,11 @@ void main()
 {
     clrscr();
     int i=1,sqr;
-    while(i>0)
+    while(i<11)
     {
         sqr =i*i;
         cout<<"Squar root of "<<i<<" is "<<sqr<<endl;
-        i++
+        i++;
     }
     getch();
 }
